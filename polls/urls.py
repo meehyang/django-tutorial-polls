e0ef027c35939fb69f7d29ba088500ac92717aef
Ichi <ichi@gelato.im>
2018-11-22 15:48:43 +0900

    URL 형태에 따른 view 매핑 설정

--- polls/urls.py
+++ polls/urls.py
@@ -2,6 +2,14 @@ from django.urls import path
 
 from . import views
 
+# 만들어진 view들을 polls.urls 모듈에 path()를 메서드를 이용해 view들을 더해준다
 urlpatterns = [
+    # /polls/ 로 호출된 경우
     path('', views.index, name='index'),
+    # /polls/5/ 형태로 호출된 경우
+    path('<int:question_id>/', views.detail, name='detail'),
+    # /polls/5/results/
+    path('<int:question_id/results/>', views.results, name='result'),
+    # /polls/5/vote/
+    path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
