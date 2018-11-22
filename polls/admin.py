3a54d5be902744c5ff5fa044decc831a4528c0b8
Ichi <ichi@gelato.im>
2018-11-22 15:48:01 +0900

    Question object 가 admin interface 를 가지고 있다는 것을 admin에게 알려주기

--- polls/admin.py
+++ polls/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Question
 
 # Register your models here.
+# Question object 가 admin interface 를 가지고 있다는 것을 admin에게 알려줘야함
+admin.site.register(Question)
